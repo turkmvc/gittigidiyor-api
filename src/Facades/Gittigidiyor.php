a6d788b43bd2fac09dab3b398f3299a6acd97271
Cuma KÖSE <25532765+turkmvc@users.noreply.github.com>
2021-01-31 16:42:11 +0300

    Create Gittigidiyor.php

---
+++ src/Facades/Gittigidiyor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace turkmvc\Gittigidiyor\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+
+class Gittigidiyor extends Facade
+{
+
+  protected static function getFacadeAccessor() { return 'gittigidiyor'; }
+}
