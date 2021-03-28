4d6603d79fbae478d5a0e28a6f30269f1c399651
Cuma KÖSE <25532765+turkmvc@users.noreply.github.com>
2021-03-28 21:20:32 +0300

    Update ExampleTest.php

--- ExampleTest.php
+++ ExampleTest.php
@@ -4,14 +4,14 @@ include "GittiGidiyor.php";
 
 use turkmvc\GittiGidiyor\GittiGidiyor;
 
-$conf 					= [];
-$conf['apikey'] 		= '****';
-$conf['secret_key'] 	= '****';
-$conf['auth_user'] 		= '****';
-$conf['nick'] 			= '****';
-$conf['password'] 		= '****';
+$conf                   = [];
+$conf['apikey'] 		    = '****';
+$conf['secret_key']     = '****';
+$conf['auth_user'] 	    = '****';
+$conf['nick'] 			    = '****';
+$conf['password'] 	    = '****';
 $conf['auth_password'] 	= '****';
-$conf['lang'] 			= 'tr';
+$conf['lang'] 			    = 'tr';
 
 $gg = new GittiGidiyor($conf);
 
